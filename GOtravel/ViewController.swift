023df5b66d740f2aebd3f2ee7376c91660096554
Haeuncs <sch_haeun@naver.com>
2019-01-09 12:33:13 +0900

    Initial Commit

---
+++ GOtravel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GOtravel
+//
+//  Created by OOPSLA on 09/01/2019.
+//  Copyright © 2019 haeun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
